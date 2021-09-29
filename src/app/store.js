591d82c62b477399f218dcfc8260fb859c9f9a0a
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2021-09-29 18:28:59 +0530

    adding news feature

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { cryptoApi } from "../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
+
 
 export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+
     },
 })
 
