0b7f4795f3b7f69681de528486ef5e7a2241528d
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2021-10-01 21:08:09 +0530

    new API header

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,10 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col, Row, Typography, Select } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+
+const { Title, Text } = Typography;
+const { Option } = Select;
+
 
 const CryptoDetails = () => {
+    const { coinId } = useParams();
+    const [timePeriod, setTimePeriod] = useState('7d');
+    const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
+    const cryptoDetails = data?.data?.coin;
+
+    const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+
+    // const stats = [
+    //   { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+    //   { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+    //   { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+    //   { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+    //   { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+    // ];
+  
+    // const genericStats = [
+    //   { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+    //   { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+    //   { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+    //   { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+    //   { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+    // ];
+
+
+    console.log(data);
+   
+    
     return (
-        <div>
-            CryptoDetails
-        </div>
+        <Col className="coin-details-container">
+          <Col className="coin-heading-container">
+   
+                {cryptoDetails.name} ({ cryptoDetails.slug }) Price
+     
+          </Col>
+        </Col>
     )
 }
 
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -13,13 +13,16 @@ export const cryptoApi = createApi({
   reducerPath: "cryptoApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
-    getCryptos: builder.query({
+    getCryptos: builder.query({ 
       query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
     })
   })
 });
 
 export const {
   // use____Query = to get the data
-  useGetCryptosQuery,
+  useGetCryptosQuery, useGetCryptoDetailsQuery
 } = cryptoApi;
