eb4dcdb0d59608a6cfe74ffc2087bcc2c726f737
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2021-10-01 21:56:58 +0530

    Crypto details stats

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -19,32 +19,52 @@ const CryptoDetails = () => {
 
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
-    // const stats = [
-    //   { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
-    //   { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
-    //   { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
-    //   { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
-    //   { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
-    // ];
+    const stats = [
+      { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+      { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+      { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+      { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+      { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+    ];
   
-    // const genericStats = [
-    //   { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
-    //   { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-    //   { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-    //   { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
-    //   { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
-    // ];
+    const genericStats = [
+      { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+      { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+      { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+      { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+      { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+    ];
 
 
-    console.log(data);
+
    
     
     return (
         <Col className="coin-details-container">
           <Col className="coin-heading-container">
-   
+            <Title level={2} className="coin-name">
                 {cryptoDetails.name} ({ cryptoDetails.slug }) Price
-     
+            </Title>
+            <p>
+               {CryptoDetails.name} live price in US dollars.
+               View value statistics, marketCap and supply.
+            </p>
+          </Col>
+          <Select defaultValue="7d" className="select-timeperiod" placeholder="Select Time Period" onChange={(value) => setTimePeriod(value)}>
+            {time.map((option) => <Option key={date}>{date}</Option>)}
+          </Select>
+          { /* Line chart component*/}
+          <Col className="stats-container">
+             <Col className="coin-value-statistics">
+                <Col className="coin-value">
+                 <Title level={3} className="coin-detailes-heading">
+                   {cryptoDetails.name} Value statistics
+                 </Title>
+                 <p>
+                    An overview showing the stats of {cryptoDetails.name}                 
+                 </p>
+                </Col>
+             </Col>
           </Col>
         </Col>
     )
